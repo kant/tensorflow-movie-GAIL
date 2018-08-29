2468aa6d83cd7279e15f53ae8d8c4e0c15c41d98
ykamikawa <ytkmkw@gmail.com>
2018-08-29 10:28:35 +0900

    fix

--- run_gail.py
+++ run_gail.py
@@ -19,10 +19,8 @@ def argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', type=str, help='path to data',
             default='../../dataset/mnist_test_seq.npy')
-    parser.add_argument('--logdir', type=str, help='log directory',
-            default='log')
-    parser.add_argument('--savedir', type=str, help='save directory',
-            default='trained_models')
+    parser.add_argument('--outdir', type=str, help='output directory',
+            default='out')
     parser.add_argument('--algo', type=str, help='algo name, ppo or trpo',
             default='ppo')
     parser.add_argument('--iteration', type=int, help='iteration',
@@ -49,24 +47,15 @@ def argparser():
 
 
 def main(args):
-    if not os.path.exists(args.logdir):
-        os.makedirs(args.logdir)
-    if not os.path.exists(args.savedir):
-        os.makedirs(args.savedir)
+    if not os.path.exists(args.outdir):
+        os.makedirs(args.outdir)
 
-    # create trained model dir
-    # ckpt counter
-    ckpt_counter = len([ckpt_dir for ckpt_dir in os.listdir(args.savedir) if args.algo in ckpt_dir])
-    model_dir = os.path.join(args.savedir, args.algo + '_' + str(ckpt_counter + 1))
-    if not os.path.exists(model_dir):
-        os.makedirs(model_dir)
-
-    # create log dir
-    # log counter
-    log_counter = len([log_dir for log_dir in os.listdir(args.logdir) if args.algo in log_dir])
-    log_dir = os.path.join(args.logdir, args.algo + '_' + str(log_counter + 1))
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
+    # Prepare output directory
+    # output directory counter
+    out_counter = len([None for out in os.listdir(args.outdir) if args.algo in out])
+    out = os.path.join(args.outdir, args.algo + '_' + str(out_counter + 1))
+    if not os.path.exists(out):
+        os.makedirs(out)
 
     config = {'data': args.data_path,
             'algo': args.algo,
@@ -83,17 +72,17 @@ def main(args):
             'vf_clip': args.vf_clip,
             'leaky': args.leaky,
             'spectral normalization': args.sn}
-    with open(os.path.join(log_dir, 'config.json'), 'w') as f:
+    with open(os.path.join(out, 'config.json'), 'w') as f:
         f.write(json.dumps(config, indent=4))
 
-    # moving mnist 読み込み
+    # Load moving mnist
     data = np.load(args.data_path)
     obs_shape = [3, args.obs_size, args.obs_size, 1]
-    # generator
+    # Start generator
     gen = generator(data,
             batch_size=args.batch_size, img_size=args.obs_size)
 
-    # policy net
+    # Build policy network
     Policy = Policy_dcgan(
             'policy',
             obs_shape=obs_shape,
@@ -106,6 +95,8 @@ def main(args):
             batch_size=args.batch_size,
             decode=True,
             leaky=args.leaky)
+
+    # Build reinforcement agent
     if args.algo == 'ppo':
         print('Building PPO Agent')
         Agent = PPOTrain(
@@ -133,7 +124,7 @@ def main(args):
     else:
         raise ValueError('invalid algo name')
 
-    # discriminator
+    # Build Discriminator
     if args.sn:
         print('Building SNGAN Descriminator')
         D = SNDiscriminator(obs_shape=obs_shape,
@@ -144,7 +135,7 @@ def main(args):
             batch_size=args.batch_size,
             leaky=args.leaky)
 
-    # tensorflow saver
+    # Create tensorflow saver
     saver = tf.train.Saver()
     # session config
     config = tf.ConfigProto(
@@ -152,21 +143,21 @@ def main(args):
                 visible_device_list=args.gpu_num,
                 allow_growth=True
                 ))
-    # start session
+    # Start session
     with tf.Session(config=config) as sess:
-        # summary writer
-        writer = tf.summary.FileWriter(log_dir, sess.graph)
-        # initialize Session
+        # Summary writer
+        writer = tf.summary.FileWriter(out, sess.graph)
+        # Initialize Session
         sess.run(tf.global_variables_initializer())
 
-        # initialize learningrate
+        # Initialize learningrate
         lr = args.initial_lr
-        # episode loop
+        # Start episode roop
         for iteration in tqdm(range(1, args.iteration+1)):
             if iteration % args.lr_schedules == 0:
                 lr = lr / 10
                 print('Decay learning rate')
-            # create batch 0~1
+            # Greate batch 0~1
             expert_batch = next(gen)
             # first 3 frame
             agent_batch = expert_batch[:, :3, :, :, :]
@@ -181,22 +172,22 @@ def main(args):
             rewards = []
             v_preds = []
             run_policy_steps = 0
-            # run episode
+            # Run episode
             while True:
-                # episodeの各変数を追加
+                # Add episode
                 observations.append(agent_batch)
 
-                # inference action(next frame) and value
+                # Inference action(next frame) and value
                 act, v_pred = Policy.act(obs=agent_batch)
                 v_preds.append(v_pred)
 
-                # get expert actions
+                # Get expert actions
                 expert_act = expert_batch[:, run_policy_steps+3, :, :, :]
                 expert_act = np.reshape(expert_act,
                         (args.batch_size, args.obs_size * args.obs_size))
                 expert_actions.append(expert_act)
 
-                # create next_obs
+                # Create next_obs
                 act = np.reshape(act,
                         (args.batch_size, 1, args.obs_size, args.obs_size, 1))
                 agent_batch_next = np.concatenate(
@@ -209,38 +200,27 @@ def main(args):
                     v_preds_next = v_preds[1:] + [np.zeros(v_preds[0].shape)]
                     break
                 else:
-                    # updata observations by old observations
+                    # Updata observations by old observations
                     agent_batch = agent_batch_next
 
-            # discriminator
+            # Discriminator
             D_step = args.D_step
-            # 動画の全タイムステップで学習
             for i, _ in enumerate(observations):
-                # get observations of expert and policy
+                # Get observations of expert and policy
                 expert_obs = expert_batch[:, i:i+3, :, :, :]
                 expert_obs_next = expert_batch[:, i+1:i+4, :, :, :]
                 agent_obs = observations[i]
                 agent_obs_next = next_observations[i]
-                # train discriminator
+                # Train discriminator
                 _, D_loss = D.train(expert_s=expert_obs,
                         expert_s_next=expert_obs_next,
                         agent_s=agent_obs,
                         agent_s_next=agent_obs_next,
                         lr=args.initial_lr)
             print('D_loss: ', D_loss)
-            '''
-            # get Discriminator summary
-            D_summary = D.get_summary(
-                    expert_s=expert_batch[:,6:9,:,:,:],
-                    expert_s_next=expert_batch[:,7:10,:,:,:],
-                    agent_s=observations[-1],
-                    agent_s_next=next_observations[-1]
-                    lr=args.initial_lr)
-            writer.add_summary(D_summary, iteration)
-            '''
 
-            # updata policy using Agent
-            # get d_rewards from discrminator
+            # Updata policy using Agent
+            # Get d_rewards from discrminator
             d_rewards = []
             for i, _ in enumerate(observations):
                 d_reward = D.get_rewards(agent_s=observations[i],
@@ -257,14 +237,14 @@ def main(args):
                         simple_value=sum(episode_d_rewards))]),
                     iteration)
 
-            # get generalized advantage estimator
+            # Get generalized advantage estimator
             gaes = Agent.get_gaes(rewards=d_rewards, v_preds=v_preds,
                     v_preds_next=v_preds_next)
             #gaes = (gaes - gaes.mean()) / (gaes.std() + 1e-5)
 
-            # train Agent
+            # Train Agent
             Agent_step = args.G_step
-            # assign parameters to old policy
+            # Assign parameters to old policy
             Agent.assign_policy_parameters()
             total_loss = 0
             clip_loss = 0
@@ -272,7 +252,7 @@ def main(args):
             entropy_loss = 0
             l1_loss = 0
             for i, _ in enumerate(observations):
-                # run ppo train operation
+                # Run ppo train operation
                 losses = Agent.train(
                         obs=observations[i],
                         gaes=gaes[i],
@@ -285,14 +265,7 @@ def main(args):
                 vf_loss += losses[3]
                 entropy_loss += losses[4]
                 l1_loss += losses[5]
-                '''
-                gradients = Agent.get_grad(
-                        obs=observations[i],
-                        gaes=gaes[i],
-                        rewards=d_rewards[i],
-                        v_preds_next=v_preds_next[i],
-                        lr=lr)
-                '''
+
             total_loss /= len(observations)
             clip_loss /= len(observations)
             vf_loss /= len(observations)
@@ -301,7 +274,7 @@ def main(args):
             print('total_loss: {}, clip_loss: {}, vf_loss: {}, entropy_loss: {} l1_loss: {}'.format(
                 total_loss, clip_loss, vf_loss, entropy_loss, l1_loss))
 
-            # get Agent summary
+            # Get agent summary
             Agent_summary = Agent.get_summary(
                     obs=observations[-1],
                     gaes=gaes[-1],
@@ -309,12 +282,11 @@ def main(args):
                     v_preds_next=v_preds_next[-1],
                     expert_act=expert_actions[-1],
                     lr=lr)
-            # add Agent summary
             writer.add_summary(Agent_summary, iteration)
 
-            # save trained model
+            # Save trained model
             if iteration % args.frequency == 0:
-                saver.save(sess, model_dir+'/model-{}.ckpt'.format(iteration))
+                saver.save(sess, out+'/model-{}.ckpt'.format(iteration))
         writer.close()
 
 
